c8d310519998aed8693741e57d034118174c6b16
Syed Asif <syedasif.work@gmail.com>
2022-08-03 18:30:49 +0530

    Part2: Exercises 2.1.-2.5. Completed

---
+++ part2/courseinfo/src/components/Course.jsx
@@ -0,0 +1,30 @@
+const Header = ({course}) => <h2>{course.name}</h2>
+
+const Part = ({part}) => <p>{part.name} {part.exercises}</p>
+
+const Content = ({parts}) => {
+  return (
+    <div>
+      {parts.map((part) => <Part key={part.id} part={part} />)}
+    </div>
+  )
+}
+
+// const total = parts.reduce((a,b) => {
+//     console.log(a)
+//     return a.exercises + b
+// }, 0)
+
+const Total = ({parts}) => <h3>Number of exercises {parts.reduce((a,b) => a + b.exercises, 0)}</h3>
+
+const Course = ({course}) => {
+    return (
+        <div>
+            <Header course={course} />
+            <Content parts={course['parts']} />
+            <Total parts={course['parts']} />
+        </div>
+    )
+}
+
+export default Course;
